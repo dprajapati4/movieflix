66b7bce628ec690496819339c13fc4c34479aad1
Deep <dprajap2@gmail.com>
2024-08-25 14:07:38 -0400

    add find movie by genreids

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+import {} from "dotenv/config";
 const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
 const MOVIE_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 const baseRequestParams = `?api_key=${MOVIE_API_KEY}`;
@@ -23,14 +23,48 @@ export const getGenres = async () => {
   }
 };
 
-// get movies by genre
+// get movies by genreIds /include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28,35
+
+export const getMoviesByGenreIds = async ({
+  genreIds,
+  adult = false,
+  page = 1,
+}) => {
+  const moviesByGenreIdEndpoint = "/discover/movie";
+  const requestParams = `&include_adult=${adult}&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genreIds}`;
+
+  const urlToFetch =
+    TMDB_API_BASE_URL +
+    moviesByGenreIdEndpoint +
+    baseRequestParams +
+    requestParams;
+
+  if (!genreIds) {
+    return new Error("No movie genre entered.");
+  }
+
+  try {
+    const response = await fetch(urlToFetch, {
+      method: "GET",
+      headers: { accept: "application/json" },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+
+      return movies;
+    }
+  } catch (err) {
+    console.log(`Error finding movies with genre ids ${genreIds}`);
+  }
+};
 
 // get movie by name query=blue&include_adult=false&page=1
-const getMovie = async ({ name, adult = false, page = 1 }) => {
+export const getMovie = async ({ name, adult = false, page = 1 }) => {
   if (!name) {
     return new Error("No movie search term entered.");
   }
-  const getMovieEndpoint = "/search/movie";
+  const getMovieEndpoint = "/search/movie?";
   const requestParams = `&query=${name}&include_adult=${adult}&page=${page}`;
   const urlToFetch =
     TMDB_API_BASE_URL + getMovieEndpoint + baseRequestParams + requestParams;
