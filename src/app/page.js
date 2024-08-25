e526ac5c5b46982ba798798dadd938d06080568c
Deep <dprajap2@gmail.com>
2024-08-25 11:32:14 -0400

    add genre selector

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getGenres } from "@/utils/api";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  useEffect(() => {
+    const loadGenres = async () => {
+      try {
+        const genreData = await getGenres();
+        setGenres(genreData);
+      } catch (err) {
+        console.log("Error loading genres", err);
+      }
+    };
+
+    loadGenres();
+  }, []);
+
+  // console.log('selected genre', selectedGenre)
   return (
-   <div>
-    <h1>MovieFlix</h1>
-   </div>
+    <div>
+      <div className="heading">
+        <h1>MovieFlix</h1>
+      </div>
+      <div>
+        <h2>Find movies you want to see</h2>
+      </div>
+      <div>
+        <p>Find Movies by Genre</p>
+        {genres?.length > 1 ? (
+          <select>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.id}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <div>loading </div>
+        )}
+      </div>
+    </div>
   );
 }
